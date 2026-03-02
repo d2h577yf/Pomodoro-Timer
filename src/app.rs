86285dfb29785912df196c44606bb2c89ab6e2ed
wanghongyu <wanghongyu1028@foxmail.com>
2026-03-02 21:33:46 +0800

    base

---
+++ src/app.rs
@@ -0,0 +1,136 @@
+use std::sync::Arc;
+use eframe::{egui, Frame};
+use std::time::{Duration,Instant};
+use egui::Context;
+
+#[derive(PartialEq, Clone, Copy)]
+enum RunningMode {
+    Up,
+    Down,
+    Loop,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum TimerState {
+    Focus,
+    Break,
+}
+pub struct PomodoroTimer {
+    is_running: bool,
+    mode: RunningMode,
+    focus_duration: Duration,
+    break_duration : Duration,
+    current_time: Duration,
+    last_tick: Option<Instant>,
+    state : TimerState,
+}
+
+impl Default for PomodoroTimer {
+    fn default() -> Self {
+        PomodoroTimer {
+            is_running : false,
+            mode : RunningMode::Down,
+            focus_duration : Duration::from_secs(25 * 60),
+            break_duration : Duration::from_secs(5 * 60),
+            current_time : Duration::from_secs(25 * 60),
+            last_tick : None,
+            state : TimerState::Focus,
+        }
+    }
+}
+
+impl PomodoroTimer {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self{
+        Self::set_font_custom(&cc.egui_ctx);
+        Self::default()
+    }
+
+    fn set_font_custom(ctx : &egui::Context) {
+        let mut fonts = egui::FontDefinitions::default();
+
+        fonts.font_data.insert(
+            "my_chinese_font".to_owned(),
+            Arc::from(egui::FontData::from_static(include_bytes!("../assets/SourceHanSansCN-Medium.otf"))),
+        );
+
+        fonts
+            .families
+            .entry(egui::FontFamily::Proportional)
+            .or_default()
+            .insert(0,"my_chinese_font".to_owned());
+
+        ctx.set_fonts(fonts)
+    }
+
+    fn tick(&mut self) {
+        if !self.is_running {
+            self.last_tick = None;
+            return;
+        }
+
+        let now = Instant::now();
+
+        if let Some(last) = self.last_tick {
+           let  elapsed = now.duration_since(last);
+
+            match self.mode {
+                RunningMode::Down => {
+                    self.current_time = self.current_time.saturating_sub(elapsed);
+
+                    if self.current_time.is_zero() {
+                        self.is_running = false;
+                        self.last_tick = None;
+                        return;
+                    }
+                }
+
+                RunningMode::Up => {
+                    self.current_time = self.current_time.saturating_add(elapsed);
+                }
+
+                RunningMode::Loop => {
+                    self.current_time = self.current_time.saturating_sub(elapsed);
+                    if self.current_time.is_zero() {
+                        match self.state {
+                            TimerState::Focus => {
+                                self.state = TimerState::Break;
+                                self.current_time = self.break_duration;
+                            }
+                            TimerState::Break => {
+                                self.state = TimerState::Focus;
+                                self.current_time = self.focus_duration;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        self.last_tick = Some(now);
+    }
+}
+
+impl eframe::App for PomodoroTimer {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        self.tick();
+
+        egui::CentralPanel::default().show(ctx , |ui| {
+           let secs = self.current_time.as_secs();
+
+            ui.label(format!("{}秒",secs));
+
+            if ui.button("开始/暂停").clicked() {
+                self.is_running = !self.is_running;
+
+                if self.is_running {
+                    self.last_tick = Some(Instant::now());
+                }
+            }
+        });
+
+        if self.is_running {
+            ctx.request_repaint();
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use Pomodoro_Timer::PomodoroTimer;
+
+fn main() -> eframe::Result<()>  {
+    let options =  eframe::NativeOptions {
+        viewport : eframe::egui::ViewportBuilder::default().with_inner_size([300.0,200.0]),
+        multisampling : 4,
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "我的番茄钟",
+        options,
+        Box::new(|cc| Ok(Box::new(PomodoroTimer::new(cc)))),
+    )
 }
